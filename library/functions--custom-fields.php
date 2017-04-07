63d6d923a38a2bf5e921173b48dba9165fa7c826
joshre <joshre@me.com>
2017-04-07 14:36:47 -0700

    Menus

--- library/functions--custom-fields.php
+++ library/functions--custom-fields.php
@@ -19,8 +19,38 @@ function prepareHomePageFields()
         'title'       => get_field('field_58a785bf9d01f'),
         'description' => get_field('field_58a785d29d020'),
     );
+
+    if (have_rows('field_58e8016eb545a')) {
+        $food = array();
+        while (have_rows('field_58e8016eb545a')) {
+            the_row();
+            $food[] = array(
+                'item'        => get_sub_field('field_58e801f2b545b'),
+                'description' => get_sub_field('field_58e801fcb545c'),
+            );
+        }
+    } else {
+        $food = null;
+    }
+    if (have_rows('field_58e8022caa428')) {
+        $cocktails = array();
+        while (have_rows('field_58e8022caa428')) {
+            the_row();
+            $cocktails[] = array(
+                'item'        => get_sub_field('field_58e8022daa429'),
+                'description' => get_sub_field('field_58e8022daa42a'),
+            );
+        }
+    } else {
+        $cocktails = null;
+    }
+    $menu = array(
+        'food'      => $food,
+        'cocktails' => $cocktails,
+    );
     $section = array(
         'email' => $email,
+        'menu'  => $menu,
     );
     return $section;
 }
