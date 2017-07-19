051ce240cd307daa424be930b1ac1777906f1ae6
joshre <joshre@me.com>
2017-07-19 14:43:38 -0700

    Scroll so nice you'll click() it twice

--- static/site.js
+++ static/site.js
@@ -44,3 +44,18 @@ jQuery(document).ready(function($) {
         // nextArrow: $next
     });
 });
+// Scroll so nice you'll click() it twice
+jQuery(function() {
+  jQuery('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = jQuery(this.hash);
+      target = target.length ? target : jQuery('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        jQuery('html, body').animate({
+          scrollTop: target.offset().top
+        },300, "swing");
+        return false;
+      }
+    }
+  });
+});
